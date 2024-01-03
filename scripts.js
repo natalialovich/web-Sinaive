0556ff0a0dc7be65aa7fa7b6cbb038d75ef44e5c
natalialovich <63924218+natalialovich@users.noreply.github.com>
2024-01-03 15:28:41 +0100

    videos page + menu + mobile css

--- scripts.js
+++ scripts.js
@@ -1,20 +1,3 @@
-// async function fetchYTData(){
-//   const API_KEY = 'AIzaSyD8PR_Fvzhaw2KeExiT_MstQPNZre96frs';
-//   const API_URL = 'https://www.googleapis.com/youtube/v3';
-
-//   const response = await fetch( `https://youtube.googleapis.com/youtube/v3/search?part=snippet&channelId=UCw2yAkSJqcJ3poiZEkSQK2A&key=${API_KEY}`
-
-//   );
-
-//   const data = await response.json();
-//   console.log(data)
-
-//   return data
-
-// }
-// fetchYTData();
-// let videos = '';
-
 // Loads the IFrame Player API code asynchronously
 
 let tag = document.createElement("script");
@@ -24,41 +7,41 @@ let firstScriptTag = document.getElementsByTagName("script")[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
 // This function creates an <iframe> (and YouTube player)
-// after the API code downloads.
 
 let player;
+const videos = ["vKWYBvbiM8Y", "4v6Blr4itQA", "foULVTzG0iA", "eP2bdPNW72Y"];
 
 let isMobile = window.innerWidth < 666;
 function onYouTubeIframeAPIReady() {
+  let playerHeight = isMobile ? 460 : 720;
 
-  let playerHeight = isMobile ? 460 : 720; // Establece la altura deseada para móviles y no móviles
-
-  player = new YT.Player("player", {
-    height: playerHeight,
-    width: "1280",
-    videoId: "vKWYBvbiM8Y",
-    playerVars: {
-      controls: 0,
-      rel: 0,
-    },
-    events: {
-      onReady: onPlayerReady,
-      onStateChange: onPlayerStateChange,
-    },
+  videos.forEach((videoInfo) => {
+    const values = videoInfo;
+    createYouTubePlayer(values);
   });
+
+  function createYouTubePlayer(videoInfo) {
+    console.log(videoInfo);
+
+    player1 = new YT.Player(videoInfo, {
+      height: playerHeight,
+      width: "100%",
+      videoId: videoInfo,
+      playerVars: {
+        controls: 0,
+        rel: 0,
+      },
+      events: {
+        onReady: onPlayerReady,
+        onStateChange: onPlayerStateChange,
+      },
+    });
+  }
 }
 
-function onPlayerReady(event) {
-  player.mute();
-}
+function onPlayerReady(event) {}
 
-// let done = false;
-
-function onPlayerStateChange(event) {
-  // if (event.data == YT.PlayerState.PLAYING && !done) {
-  //   done = true;
-  //
-}
+function onPlayerStateChange(event) {}
 
 function stopVideo() {
   player.stopVideo();
@@ -73,17 +56,31 @@ const nav = document.querySelector("nav");
 const logo = document.querySelector(".logo p");
 const container = document.querySelector(".container");
 const btnMobile = document.getElementById("vinyl-record");
+const discoItem = document.getElementById("disco-nav-item");
+const dot = document.querySelector(".dot-nav");
 
 // This code creates a parallax effect on the album items
 
 let scrollStart = window.scrollY;
+let parallaxActive = true;
 
 const parallax = () => {
-  if (scrollStart > 860) {
+  if (parallaxActive && scrollStart > 860) {
     if (scrollStart < window.scrollY) {
       parallaxBackground.scrollTop += 3;
     } else {
       parallaxBackground.scrollTop -= 3;
+    }
+    if (!isMobile) {
+      if (scrollStart > 1500) {
+        discoItem.firstChild.style.color = "#e19e2d";
+        discoItem.firstChild.style.textDecoration = "underline";
+        dot.style.display = "inline-block";
+      } else {
+        discoItem.firstChild.style.color = "white";
+        discoItem.firstChild.style.textDecoration = "none";
+        dot.style.display = "none";
+      }
     }
   }
   scrollStart = window.scrollY;
@@ -93,18 +90,35 @@ if (parallaxBackground) {
   window.addEventListener("scroll", parallax);
 }
 
-//change logo size depending on the page
+//Discography element scroll
 
-if (window.location.pathname !== "/index.html") {
-  logo.style.fontSize = "7cqw";
-  logo.style.margin = "0 4% 4% 4%";
+document.addEventListener("DOMContentLoaded", function () {
+  const refPage = document.referrer.split("/").pop();
+
+  if (
+    refPage == "videos.html" ||
+    refPage == "shop.html" ||
+    refPage == "about.html"
+  ) {
+    parallaxActive = false;
+    setTimeout(() => {
+      parallaxActive = true;
+    }, 1000);
+  }
+});
+
+function scrollToItem() {
+  parallaxActive = false;
+  if (isMobile) {
+    toggleMenu();
+  }
+  discoItem.firstChild.scrollIntoView({ behavior: "smooth" });
+  setTimeout(() => {
+    parallaxActive = true;
+  }, 600);
 }
 
-//About section shorten height
-
-if (window.location.pathname === "/About.html") {
-  container.style.minHeight = "auto";
-}
+discoItem.firstChild.addEventListener("click", scrollToItem);
 
 //Blur navigation on scroll
 
@@ -114,15 +128,15 @@ if (!isMobile) {
     const padding = 10;
     const contrast = Math.max(100, scrollPosition / 9);
     const blur = Math.min(4, scrollPosition / 400);
-  
+
     let value = Math.max(1, padding - (scrollPosition / 100).toFixed(2));
-  
+
     nav.style.backdropFilter = `blur(${blur}px) grayscale(75%) contrast(${contrast}%)`;
     nav.style.paddingTop = `${value}px`;
     nav.style.paddingBottom = `${value}px`;
     menu.style.paddingTop = `${value}px`;
     menu.style.paddingBottom = `${value}px`;
-  
+
     navItems.forEach((navItem) => {
       navItem.style.paddingTop = `${value}px`;
       navItem.style.paddingBottom = `${value}px`;
@@ -130,26 +144,20 @@ if (!isMobile) {
   });
 }
 
-
-
 //Mobile menu button
 
-btnMobile.addEventListener('click', toggleMenu);
+btnMobile.addEventListener("click", toggleMenu);
 
 function toggleMenu() {
-
-  nav.classList.toggle('active-mobile')
-
+  nav.classList.toggle("active-mobile");
 }
-
-
 
 //Album back covers mouse effect and noise
 
 const backCoverEffect = (e) => {
   noise.style.display = "block";
   const targetImage = e.target;
-  const nextSibling = e.target.nextElementSibling;
+  const nextSibling = e.target.parentNode.nextElementSibling;
 
   const targetRect = targetImage.getBoundingClientRect();
   const siblingRect = nextSibling.getBoundingClientRect();
@@ -172,20 +180,48 @@ const backCoverEffect = (e) => {
   nextSibling.style.left = siblingCenterX + "px";
   nextSibling.style.top = siblingCenterY + "px";
 
-  if (nextSibling) {
-    if (nextSibling.style.visibility !== "visible") {
-      nextSibling.style.visibility = "visible";
-    }
-  }
+  nextSibling.style.visibility = "visible";
 };
 
+function mobileAlbum(e) {
+  noise.style.display = "block";
+
+  const nextSibling = e.target.parentNode.nextElementSibling;
+  const targetImage = e.target;
+
+  const targetRect = targetImage.getBoundingClientRect();
+
+  const targetCenterX = targetRect.left + targetRect.width / 10;
+  const targetCenterY = targetRect.top + targetRect.height / 10;
+
+  const siblingCenterX = targetCenterX;
+  const siblingCenterY = targetCenterY;
+
+  nextSibling.style.width = "250px";
+  nextSibling.style.left = siblingCenterX + "px";
+  nextSibling.style.top = siblingCenterY + "px";
+
+  nextSibling.style.visibility = "visible";
+}
+
 albumCovers.forEach((albumCover) => {
-  albumCover.addEventListener("mousemove", backCoverEffect);
-  albumCover.addEventListener("mouseleave", (e) => {
-    const nextSibling = e.target.nextElementSibling;
-    if (nextSibling) {
-      nextSibling.style.visibility = "hidden";
-      noise.style.display = "none";
-    }
-  });
+  if (!navigator.maxTouchPoints > 0) {
+    albumCover.addEventListener("mousemove", backCoverEffect);
+    albumCover.addEventListener("mouseleave", (e) => {
+      const nextSibling = e.target.parentNode.nextElementSibling;
+      if (nextSibling) {
+        nextSibling.style.visibility = "hidden";
+        noise.style.display = "none";
+      }
+    });
+  } else {
+    albumCover.addEventListener("touchstart", mobileAlbum);
+    albumCover.addEventListener("touchend", (e) => {
+      const nextSibling = e.target.parentNode.nextElementSibling;
+      if (nextSibling) {
+        nextSibling.style.visibility = "hidden";
+        noise.style.display = "none";
+      }
+    });
+  }
 });
